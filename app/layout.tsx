7486e191d2200873f56f934cd4a79fade2803d99
Betselot Tadele <betselottadeleaba.com>
2026-08-19 08:56:55 -0800

    home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import "./globals.css";
+
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,3 +1,3 @@
-export default function productspage(){
-    return <h1>this is the products page</h1>
+export default function Home() {
+  return <h1>Hello World</h1>
 }
